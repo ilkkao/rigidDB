8b9270ab27e609fe8c9e27f9af0757894c98a9d5
Ilkka Oksanen <iao@iki.fi>
2015-10-24 13:51:22 +0200

    Setup mocha, chai and karma

---
+++ test/basic-test.js
@@ -0,0 +1,10 @@
+var expect = chai.expect;
+
+describe('Hello Greeter for myapp', function() {
+    describe('test greet', function() {
+        it("should be return hello world", function() {
+            console.log('hello');
+            expect(1).to.equal(1);
+        });
+    });
+});
