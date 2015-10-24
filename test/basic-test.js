89f83c62d6b7e092d2462dfc974e85300bbd94cc
Ilkka Oksanen <iao@iki.fi>
2015-10-24 14:23:41 +0200

    Remove Karma, it's not needed

--- test/basic-test.js
+++ test/basic-test.js
@@ -1,9 +1,8 @@
-var expect = chai.expect;
+var expect = require('chai').expect;
 
 describe('Hello Greeter for myapp', function() {
     describe('test greet', function() {
         it("should be return hello world", function() {
-            console.log('hello');
             expect(1).to.equal(1);
         });
     });
