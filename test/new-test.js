15e99077383a67e573efca16eb137c7666054e39
Ilkka Oksanen <iao@iki.fi>
2015-11-11 08:24:28 +0100

    Fix quotes

--- test/new-test.js
+++ test/new-test.js
@@ -4,49 +4,49 @@ const expect = require('chai').expect,
       ObjectStore = require('../index');
 
 describe('Constructor', function() {
-    it("Missing prefix parameter throws", function() {
+    it('Missing prefix parameter throws', function() {
         expect(function() {
             new ObjectStore();
         }).to.throw('Invalid prefix.');
     });
 
-    it("Invalid prefix parameter throws", function() {
+    it('Invalid prefix parameter throws', function() {
         expect(function() {
             new ObjectStore('!notvalid');
         }).to.throw('Invalid prefix.');
     });
 
-    it("Missing schema parameter throws", function() {
+    it('Missing schema parameter throws', function() {
         expect(function() {
             new ObjectStore('foo');
         }).to.throw('Invalid schema.');
     });
 
-    it("Zero collections throws", function() {
+    it('Zero collections throws', function() {
         expect(function() {
             new ObjectStore('foo', {});
         }).to.throw('At least one collection must be defined.');
     });
 
-    it("Missing collection definition throws", function() {
+    it('Missing collection definition throws', function() {
         expect(function() {
             new ObjectStore('foo', { cars: {} });
         }).to.throw('Definition missing.');
     });
 
-    it("Invalid collection field name throws", function() {
+    it('Invalid collection field name throws', function() {
         expect(function() {
             new ObjectStore('foo', { cars: { definition: { 'co:lor': 'string' }} });
         }).to.throw('Invalid field name: \'co:lor\'');
     })
 
-    it("Invalid collection field type throws", function() {
+    it('Invalid collection field type throws', function() {
         expect(function() {
             new ObjectStore('foo', { cars: { definition: { color: 'wrong' }} });
         }).to.throw('Invalid type: \'wrong\'');
     });
 
-    it("Missing unique property in index definition throws", function() {
+    it('Missing unique property in index definition throws', function() {
         expect(function() {
             new ObjectStore('foo', {
                 cars: {
@@ -59,7 +59,7 @@ describe('Constructor', function() {
         }).to.throw('Invalid or missing index unique definition');
     });
 
-    it("Invalid field name in index definition throws", function() {
+    it('Invalid field name in index definition throws', function() {
         expect(function() {
             new ObjectStore('foo', {
                 cars: {
@@ -73,7 +73,7 @@ describe('Constructor', function() {
         }).to.throw('Invalid index field: \'mileage\'');
     });
 
-    it("Valid parameters don't throw", function() {
+    it('Valid parameters don\'t throw', function() {
         expect(function() {
             new ObjectStore('foo', {
                 cars: {
