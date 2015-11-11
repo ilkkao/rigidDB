7e84908ebc0294ee760903fdfb5d892f839a93ab
Ilkka Oksanen <iao@iki.fi>
2015-11-11 08:45:26 +0100

    Add first create() tests

---
+++ test/create-test.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const expect = require('chai').expect,
+      Redis = require('ioredis'),
+      ObjectStore = require('../index');
+
+let redisClient = new Redis({
+    db: 15
+});
+
+let store;
+
+describe('Create', function() {
+    before(function() {
+        return redisClient.flushdb().then(function() {
+            store = new ObjectStore('foo', {
+                car: {
+                    definition: {
+                        color: 'string',
+                        mileage: 'int',
+                        convertible: 'boolean',
+                        purchaseDate: 'date'
+                    },
+                    indices: [{
+                        uniq: true,
+                        fields: [ 'purchaseDate' ]
+                    }, {
+                        uniq: false,
+                        fields: [ 'color', 'mileage', 'convertible' ]
+                    }]
+                }
+            }, {
+                db: 15
+            });
+        });
+    });
+
+    it('Fails if not all parameters are given', function() {
+        return store.create('car', {
+            color: 'blue',
+            mileage: 12345,
+            convertible: true,
+        }).then(function(result) {
+            expect(result).to.deep.equal({
+                command: 'CREATE',
+                err: 'E_PARAMS',
+                val: false
+            });
+        });
+    });
+
+    it('Redis is updated correctly ', function() {
+        return store.create('car', {
+            color: 'blue',
+            mileage: 12345,
+            convertible: true,
+            purchaseDate: new Date('Sun Nov 01 2015 17:41:24 GMT+0100 (CET)')
+        }).then(function(result) {
+            expect(result).to.deep.equal({
+                val: 1
+            });
+
+            return redisClient.hgetall('foo:car:1');
+        }).then(function(result) {
+            expect(result).to.deep.equal({
+                color: 'blue',
+                mileage: '12345',
+                convertible: 'true',
+                purchaseDate: 'Sun Nov 01 2015 17:41:24 GMT+0100 (CET)'
+            });
+
+            return redisClient.smembers('foo:car:ids');
+        }).then(function(result) {
+            expect(result).to.deep.equal([ "1" ]);
+
+            return redisClient.get('foo:car:nextid');
+        }).then(function(result) {
+            expect(result).to.equal('1');
+
+            return redisClient.hgetall('foo:car:index:purchaseDate');
+        }).then(function(result) {
+            expect(result).to.deep.equal({
+                'Sun Nov 01 2015 17::41::24 GMT+0100 (CET)': '1'
+            });
+
+            return redisClient.smembers('foo:car:index:color:convertible:mileage:blue:true:12345');
+        }).then(function(result) {
+            expect(result).to.deep.equal([ '1' ]);
+
+            return redisClient.keys('*');
+        }).then(function(result) {
+            expect(result).to.have.length(5);
+        });
+    });
+});
