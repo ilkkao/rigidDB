78208b6e39013e8158f89e8d5d3e050c0f33e07c
Ilkka Oksanen <iao@iki.fi>
2015-11-11 20:27:28 +0100

    Add get tests

---
+++ test/get-test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const expect = require('chai').expect,
+      Redis = require('ioredis'),
+      ObjectStore = require('../index');
+
+let redisClient = new Redis({
+    db: 15
+});
+
+let store;
+let id;
+
+describe('Get', function() {
+    beforeEach(function() {
+        return redisClient.flushdb().then(function() {
+            store = new ObjectStore('foo', {
+                car: {
+                    definition: {
+                        color: 'string',
+                        mileage: 'int',
+                        convertible: 'boolean',
+                        purchaseDate: 'date'
+                    },
+                    indices: [{
+                        uniq: true,
+                        fields: [ 'purchaseDate' ]
+                    }, {
+                        uniq: false,
+                        fields: [ 'color', 'mileage', 'convertible' ]
+                    }]
+                }
+            }, {
+                db: 15
+            });
+
+            return store.create('car', {
+                color: 'blue',
+                mileage: 12345,
+                convertible: true,
+                purchaseDate: new Date('Sun Nov 01 2015 17:41:24 GMT+0100 (CET)')
+            });
+        }).then(function(result) {
+            id = result.val;
+        });
+    });
+
+    it('Fails if id doesn\'t exist', function() {
+        return store.get('car', 4242).then(function(result) {
+            expect(result).to.deep.equal({
+                command: 'GET',
+                err: 'E_MISSING',
+                val: false
+            });
+
+            return redisClient.keys('*');
+        }).then(function(result) {
+            expect(result).to.have.length(5);
+        });
+    });
+
+    it('Succeeds if id exists', function() {
+        return store.get('car', id).then(function(result) {
+            expect(result).to.deep.equal({
+                val: {
+                    color: 'blue',
+                    mileage: 12345,
+                    convertible: true,
+                    purchaseDate: new Date('Sun Nov 01 2015 17:41:24 GMT+0100 (CET)')
+                }
+            });
+
+            return redisClient.keys('*');
+        }).then(function(result) {
+            expect(result).to.have.length(5);
+        });
+    });
+});
