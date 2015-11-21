7f591e07476a57d49ba2205f499bf23992b3cf9a
Ilkka Oksanen <iao@iki.fi>
2015-11-21 18:31:21 +0100

    Add getSchema tests

---
+++ test/getSchema-test.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const expect = require('chai').expect,
+      Redis = require('ioredis'),
+      ObjectStore = require('../index');
+
+let redisClient = new Redis({
+    db: 15
+});
+
+let store = new ObjectStore('foo', { db: 15 });
+
+describe('GetSchema', function() {
+    beforeEach(function() {
+        store = new ObjectStore('foo', { db: 15 });
+
+        return redisClient.flushdb();
+    });
+
+    it('Fails when schema is not set', function() {
+        return store.getSchema().then(function(result) {
+            expect(result).to.deep.equal({
+                command: 'GETSCHEMA',
+                err: 'schemaMissing',
+                val: false
+            });
+        })
+    });
+
+    it('Returns correct original schema', function() {
+        return store.setSchema(1, {
+            cars: {
+                definition: {
+                    color: 'string',
+                    year: 'int',
+                    convertible: 'boolean',
+                    purchaseDate: 'date',
+                    created: 'timestamp'
+                },
+                indices: {
+                    first: {
+                        uniq: true,
+                        fields: [ 'purchaseDate' ]
+                    },
+                    second: {
+                        uniq: false,
+                        fields: [ 'color', 'year', 'convertible' ]
+                    }
+                }
+            }
+        }).then(function(result) {
+            return store.getSchema();
+        }).then(function(result) {
+            expect(result).to.deep.equal({
+                val: {
+                    revision: 1, schema: {
+                        cars: {
+                            definition: {
+                                color: 'string',
+                                year: 'int',
+                                convertible: 'boolean',
+                                purchaseDate: 'date',
+                                created: 'timestamp'
+                            },
+                            indices: {
+                                first: {
+                                    uniq: true,
+                                    fields: [ 'purchaseDate' ]
+                                },
+                                second: {
+                                    uniq: false,
+                                    fields: [ 'color', 'year', 'convertible' ]
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        })
+    });
+});
